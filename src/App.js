a1e3b517eed8a8f746e3926b56fa285f2e20ab55
Yashika-01 <yashika.kuckian@gmail.com>
2025-01-02 17:17:41 +0530

    Update: Switched to Hash Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import Experience from './pages/Experience';
@@ -11,9 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/experience" element={<Experience/>} />
-        <Route path="/education" element={<EducationTimeline/>} />
-        <Route path="/publications" element={<Publication />}/>
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/education" element={<EducationTimeline />} />
+        <Route path="/publications" element={<Publication />} />
       </Routes>
     </Router>
   );
